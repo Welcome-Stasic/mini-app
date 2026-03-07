13c498d6b1b0ce05392947ddb53aa7d062e2be96
Stas <stanislav.glumov.2006@mail.ru>
2026-03-07 16:32:10 +0500

    mobX

--- src/components/EventsByType.tsx
+++ src/components/EventsByType.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import EventCard from "../components/EventCard";
 import type { EventItem } from "../types/events";
 import "./EventsByType.css";
@@ -85,4 +84,3 @@ function EventsByType({ type, events }: EventsByTypeProps) {
 }
 
 export default EventsByType;
-
