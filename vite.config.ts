42b178c0905faaa6d89cc0d8df0581d69dbdbf61
Stas <stanislav.glumov.2006@mail.ru>
2026-03-12 15:27:05 +0500

    fix backend

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/mini-app/",
+  base: "/",
   plugins: [react()],
   server: {
     proxy: {
