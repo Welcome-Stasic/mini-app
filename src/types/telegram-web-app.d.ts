de3ce7d17f664c100ab192dc35d3d29d9656972e
Papa-Jon <Stanislav.glumov.2006@mail.ru>
2026-04-21 15:09:36 +0500

    Update telegram-web-app.d.ts

--- src/types/telegram-web-app.d.ts
+++ src/types/telegram-web-app.d.ts
@@ -1,27 +1,27 @@
-export {};
+// export {};
 
-interface WebAppUser {
-  id: number;
-  first_name: string;
-  last_name?: string;
-  username?: string;
-  photo_url?: string;
-}
+// interface WebAppUser {
+//   id: number;
+//   first_name: string;
+//   last_name?: string;
+//   username?: string;
+//   photo_url?: string;
+// }
 
-interface WebAppInitData {
-  user?: WebAppUser;
-}
+// interface WebAppInitData {
+//   user?: WebAppUser;
+// }
 
-declare global {
-  interface Window {
-    Telegram?: {
-      WebApp: {
-        colorScheme: "light" | "dark";
-        initData: string;
-        initDataUnsafe: WebAppInitData;
-        onEvent: (eventType: string, callback: () => void) => void;
-        ready: () => void;
-      };
-    };
-  }
-}
+// declare global {
+//   interface Window {
+//     Telegram?: {
+//       WebApp: {
+//         colorScheme: "light" | "dark";
+//         initData: string;
+//         initDataUnsafe: WebAppInitData;
+//         onEvent: (eventType: string, callback: () => void) => void;
+//         ready: () => void;
+//       };
+//     };
+//   }
+// }
